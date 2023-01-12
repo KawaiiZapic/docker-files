0c19e78c1256a02d182ae6b66f1a379437d966e1
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2023-01-12 20:41:51 +0800

    update

--- entry.sh
+++ entry.sh
@@ -3,11 +3,20 @@ if [ ! $AGREE_TOS ]; then
     echo "You MUST agree Let's Encrypt TOS to continue, set AGREE_TOS to 1."
     exit 1
 fi
-P_DOMAINS=""
-for domain in $DOMAINS; do
-    P_DOMAINS="$P_DOMAINS -d $domain"
-done
+
+echo <<EOF
+dns_cloudflare_email = $EMAIL
+dns_cloudflare_api_key = $CLOUDFLARE_TOKEN
+EOF > ~/.cloudflare
+
+chmod 400 ~/.cloudfalre
+
 certbot certonly \
-    --non-interactive --agree-tos \
+    --non-interactive --agree-tos --email $EMAIL \
+    --cert-name default \
     --dns-cloudflare --dns-cloudflare-credentials ~/.cloudflare \
-    $P_DOMAINS
+    -d $DOMAINS
+
+kubectl --token=$K8S_TOKEN -n $K8S_NAMESPACE delete secret $K8S_SECRET_NAME
+kubectl --token=$K8S_TOKEN -n $K8S_NAMESPACE create secret tls $K8S_SECRET_NAME \
+    --cert=/etc/letsencrypt/live/default/fullchain.pem --key=/etc/letsencrypt/live/default/privkey.pem
