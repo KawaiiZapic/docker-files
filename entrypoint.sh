6a1f152bba59a256f1ab1356e8fb5c5363d411d9
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2023-01-14 23:19:12 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $NGINX_EXTRA_CONFIG ]; then
-    echo $NGINX_EXTRA_CONFIG > /etc/nginx/vhost_extra_config
+    echo "$NGINX_EXTRA_CONFIG" > /etc/nginx/vhost_extra_config
 fi
 
 (nginx && kill "$$") &
